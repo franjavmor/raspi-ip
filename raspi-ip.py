6cbc8f43139a26fde7c0e87280ff89b7823bf955
Francisco Javier Morales <fmorales@ar.ibm.com>
2020-05-23 19:41:01 -0300

    Update de raspi-ip.py para eliminar el break-line final.

--- raspi-ip.py
+++ raspi-ip.py
@@ -2,6 +2,7 @@ import os
 
 def raspi_ip():
     ipaddress = os.popen("hostname -I").readline()
+    ipaddress = ipaddress.replace('\n', ' ').replace('\r', '')
     return ipaddress
 
 print(raspi_ip())
